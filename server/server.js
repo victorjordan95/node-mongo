60aa798af13bff704ed33b0af2d5a7022d41a887
Victor Jordan <victorjordan95@gmail.com>
2018-10-26 23:50:38 -0300

    creating find todo by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,7 +28,24 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e)
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(400).send('Invalid ID!');
+    } else {
+        Todo.findById(id).then((todo) => {
+            if (!todo) {
+                res.status(404).send('Todo not found');
+                console.log('Todo not found');
+            }
+            res.send({todo})
+        }).catch((e) => console.log(e));
+    }
 })
+
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
